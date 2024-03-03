b0c1a7e9e62f43c41193c9173508c78d3d98d9ae
g3rion <gerard.kulawik@gmail.com>
2024-03-03 13:04:53 +0100

    first commit

---
+++ master.java
@@ -0,0 +1,13 @@
+public class master {
+    public static void main (String[] args) {
+        System.out.println("Hello, World!") ;
+    }
+}
+
+public static void main (String[] args) {
+    String firstName = "Gerard";
+    String lastName = "Kulawik" ;
+    int albumNumber = 6666 ;
+
+    System.out.println("Data:firstName + lastName + albumNumber") ;
+}
